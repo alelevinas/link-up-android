5a07de8ad7f9982a1b6745268c07f54acb9b5d3f
alejandro <levinasale@gmail.com>
2017-09-09 17:21:03 -0300

    Changed DummyContent to InterestContent

--- app/src/main/java/com/fiuba/tdp/linkup/views/FirstSignUpActivity.java
+++ app/src/main/java/com/fiuba/tdp/linkup/views/FirstSignUpActivity.java
@@ -2,18 +2,14 @@ package com.fiuba.tdp.linkup.views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.fiuba.tdp.linkup.R;
 import com.fiuba.tdp.linkup.components.InterestsFragment;
-import com.fiuba.tdp.linkup.components.dummy.DummyContent;
+import com.fiuba.tdp.linkup.components.dummy.InterestsContent;
 
 public class FirstSignUpActivity extends AppCompatActivity implements InterestsFragment.OnListFragmentInteractionListener {
 
@@ -54,7 +50,7 @@ public class FirstSignUpActivity extends AppCompatActivity implements InterestsF
 
 
     @Override
-    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+    public void onListFragmentInteraction(InterestsContent.InterestItem item) {
         return;
     }
 
