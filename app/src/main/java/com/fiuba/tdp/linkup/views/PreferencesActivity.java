e2b9c3cca2c26114929ae0ed18427d928e9e6d8b
Nico Moccagatta <nicomoccagatta@gmail.com>
2017-09-11 02:42:41 -0300

    Current preference values loaded OnResume

--- app/src/main/java/com/fiuba/tdp/linkup/views/PreferencesActivity.java
+++ app/src/main/java/com/fiuba/tdp/linkup/views/PreferencesActivity.java
@@ -6,20 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 
 import com.facebook.Profile;
 import com.fiuba.tdp.linkup.R;
 import com.fiuba.tdp.linkup.domain.ServerResponse;
-import com.fiuba.tdp.linkup.domain.UserAround;
-import com.fiuba.tdp.linkup.domain.UsersAround;
+import com.fiuba.tdp.linkup.domain.UserPreferences;
 import com.fiuba.tdp.linkup.services.UserService;
 
 import org.florescu.android.rangeseekbar.RangeSeekBar;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,13 +29,13 @@ public class PreferencesActivity extends AppCompatActivity {
     private Button linkUpPlusButton;
     private Switch likeMenSwitch;
     private Switch likeWomenSwitch;
-    private RangeSeekBar distanceRangeSeekBar;
-    private RangeSeekBar ageRangeSeekBar;
+    private RangeSeekBar<Integer> distanceRangeSeekBar;
+    private RangeSeekBar<Integer> ageRangeSeekBar;
     private Switch invisibleSwitch;
-    private RadioGroup searchingRadioGroup;
+    private RadioButton radioRelationship;
+    private RadioButton radioFriendship;
     private Button deleteAccountButton;
     private UserService userService;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +57,15 @@ public class PreferencesActivity extends AppCompatActivity {
         likeMenSwitch = (Switch) findViewById(R.id.switch_men);
         likeWomenSwitch = (Switch) findViewById(R.id.switch_women);
 
-        distanceRangeSeekBar = (RangeSeekBar) findViewById(R.id.seekbar_distance);
+        distanceRangeSeekBar = (RangeSeekBar<Integer>) findViewById(R.id.seekbar_distance);
         distanceRangeSeekBar.setSelectedMaxValue(10);
 
-        ageRangeSeekBar = (RangeSeekBar) findViewById(R.id.rgseekbar_age);
+        ageRangeSeekBar = (RangeSeekBar<Integer>) findViewById(R.id.rgseekbar_age);
 
         invisibleSwitch = (Switch) findViewById(R.id.switch_invisible);
 
-        searchingRadioGroup = (RadioGroup) findViewById(R.id.friend_relation_radiogroup);
+        radioFriendship = (RadioButton) findViewById(R.id.radio_friendship);
+        radioRelationship = (RadioButton) findViewById(R.id.radio_relationship);
 
         deleteAccountButton = (Button) findViewById(R.id.button_delete);
 
@@ -74,6 +74,46 @@ public class PreferencesActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Eliminar cuenta!!!",
                         Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Preferences view", "Refreshing current values for user "+Profile.getCurrentProfile().getId());
+        userService.getUserPreferences(Profile.getCurrentProfile().getId(), new Callback<ServerResponse<UserPreferences>>() {
+            @Override
+            public void onResponse(Call<ServerResponse<UserPreferences>> call, Response<ServerResponse<UserPreferences>> response) {
+                UserPreferences userPreferences = response.body().data;
+                if(Objects.equals(userPreferences.getGender(), "both")){
+                    likeMenSwitch.setChecked(true);
+                    likeWomenSwitch.setChecked(true);
+                }
+                if(Objects.equals(userPreferences.getGender(), "male")){
+                    likeMenSwitch.setChecked(true);
+                    likeWomenSwitch.setChecked(false);
+                }
+                if(Objects.equals(userPreferences.getGender(), "female")){
+                    likeMenSwitch.setChecked(false);
+                    likeWomenSwitch.setChecked(true);
+                }
+                distanceRangeSeekBar.setSelectedMaxValue(Integer.parseInt(userPreferences.getDistance()));
+
+                ageRangeSeekBar.setSelectedMinValue(Integer.parseInt(userPreferences.getMinAge()));
+                ageRangeSeekBar.setSelectedMaxValue(Integer.parseInt(userPreferences.getMaxAge()));
+
+                invisibleSwitch.setChecked(Objects.equals(userPreferences.getMode(),"invisible"));
+
+                if(Objects.equals(userPreferences.getSearchMode(), "couple"))
+                    radioRelationship.setChecked(true);
+                else
+                    radioFriendship.setChecked(true);
+            }
+
+            @Override
+            public void onFailure(Call<ServerResponse<UserPreferences>> call, Throwable t) {
+                Log.w("Preferences Activity", "Error: "+t.toString()+", in call: "+call.toString());
             }
         });
     }
@@ -99,7 +139,7 @@ public class PreferencesActivity extends AppCompatActivity {
         String maxAge = ageRangeSeekBar.getSelectedMaxValue().toString();
         String mode = invisibleSwitch.isChecked() ? "invisible" : "visible";
         String searchMode;
-        if(searchingRadioGroup.getCheckedRadioButtonId() == R.id.relationship)
+        if(radioRelationship.isChecked())
             searchMode = "couple";
         else
             searchMode = "friendship";
