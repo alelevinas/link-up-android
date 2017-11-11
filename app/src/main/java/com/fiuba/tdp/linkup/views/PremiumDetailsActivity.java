989fbc8a678eb61c9d3c5a5ec9f5e40f6d36c7c7
alejandro <levinasale@gmail.com>
2017-11-11 17:34:57 -0300

    Premium Payment field checks!

--- app/src/main/java/com/fiuba/tdp/linkup/views/PremiumDetailsActivity.java
+++ app/src/main/java/com/fiuba/tdp/linkup/views/PremiumDetailsActivity.java
@@ -1,12 +1,19 @@
 package com.fiuba.tdp.linkup.views;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.fiuba.tdp.linkup.R;
 
-public class PremiumDetailsActivity extends AppCompatActivity {
+public class PremiumDetailsActivity extends AppCompatActivity implements PremiumDetailsActivityFragment.OnPremiumDetailsActivityFragmentInteractionListener,
+        PremiumPaymentFragment.OnPremiumPaymentFragmentInteractionListener {
+
+    private Fragment detailsFragment = new PremiumDetailsActivityFragment();
+    private Fragment paymentFragment = new PremiumPaymentFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,37 @@ public class PremiumDetailsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        pushFragment(detailsFragment);
     }
 
+
+    /**
+     * Method to push any fragment into given id.
+     *
+     * @param fragment An instance of Fragment to show into the given id.
+     */
+    protected void pushFragment(Fragment fragment) {
+        if (fragment == null)
+            return;
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager != null) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if (ft != null) {
+                ft.replace(R.id.rootLayout, fragment);
+                ft.commit();
+            }
+        }
+    }
+
+    @Override
+    public void onOnPremiumDetailsActivityFragmentInteractionListenerInteraction() {
+        pushFragment(paymentFragment);
+    }
+
+    @Override
+    public void onPremiumPaymentFragmentInteraction() {
+        finish();
+    }
 }
--- app/src/main/java/com/fiuba/tdp/linkup/views/PremiumPaymentFragment.java
+++ app/src/main/java/com/fiuba/tdp/linkup/views/PremiumPaymentFragment.java
@@ -2,87 +2,124 @@ package com.fiuba.tdp.linkup.views;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.net.Uri;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.fiuba.tdp.linkup.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PremiumPaymentFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PremiumPaymentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PremiumPaymentFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final String TAG = "PREMIUM PAY";
+    Spinner card_type;
+    EditText card_nmbr;
+    EditText card_name;
+    EditText card_date;
+    EditText card_cvc;
+    Button pay_button;
 
-    private OnFragmentInteractionListener mListener;
+    String type;
+
+    private OnPremiumPaymentFragmentInteractionListener mListener;
 
     public PremiumPaymentFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PremiumPaymentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PremiumPaymentFragment newInstance(String param1, String param2) {
-        PremiumPaymentFragment fragment = new PremiumPaymentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_premium_payment, container, false);
+        View v = inflater.inflate(R.layout.fragment_premium_payment, container, false);
+
+        card_type = (Spinner) v.findViewById(R.id.cards_spinner);
+        card_nmbr = (EditText) v.findViewById(R.id.card_nmbr);
+        card_name = (EditText) v.findViewById(R.id.card_name);
+        card_date = (EditText) v.findViewById(R.id.card_date);
+        card_cvc = (EditText) v.findViewById(R.id.card_cvc);
+        pay_button = (Button) v.findViewById(R.id.button_pay);
+
+
+        pay_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkCardInput()) {
+                    upgradeToPremium();
+                }
+            }
+        });
+
+
+        setUpSpinner();
+
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    private void upgradeToPremium() {
+        Log.d(TAG, "Upgrading to PREMIUM");
+    }
+
+    private boolean checkCardInput() {
+        if (card_nmbr.getText().toString().length() < 16) {
+            showAlert("Atención", "Ingrese los 16 dígitos de la tarjeta sin espacios");
+            return false;
         }
+        if (card_name.getText().toString().length() < 3) {
+            showAlert("Atención", "Ingrese su nombre completo");
+            return false;
+        }
+        if (card_date.getText().toString().length() < 5) {
+            showAlert("Atención", "Ingrese la fecha de vencimiento de la forma MM/YY");
+            return false;
+        }
+        if (card_cvc.getText().toString().length() < 3) {
+            showAlert("Atención", "Ingrese los 3 números al dorso de la tarjeta");
+            return false;
+        }
+        return true;
+    }
+
+    private void setUpSpinner() {
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.credit_cards, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        card_type.setAdapter(adapter);
+
+        card_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d(TAG, (String) adapterView.getItemAtPosition(i));
+                type = (String) adapterView.getItemAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                type = "";
+            }
+        });
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnPremiumPaymentFragmentInteractionListener) {
+            mListener = (OnPremiumPaymentFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement OnPremiumPaymentFragmentInteractionListener");
         }
     }
 
@@ -92,18 +129,29 @@ public class PremiumPaymentFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void showAlert(String title, String message) {
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        // 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(message)
+                .setTitle(title);
+
+        // 3. Add the buttons
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                dialog.dismiss();
+            }
+        });
+
+        // 4. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+    }
+
+    public interface OnPremiumPaymentFragmentInteractionListener {
+        void onPremiumPaymentFragmentInteraction();
     }
 }
